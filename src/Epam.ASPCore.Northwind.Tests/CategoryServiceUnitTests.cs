34b2b7eba6e6e24d9ee33e977c6cf25aae31a4d4
Pavel <pavel.veselov@hotmail.com>
2019-10-15 22:34:20 +0300

    Added tests for catigory service

--- src/Epam.ASPCore.Northwind.Tests/CategoryServiceUnitTests.cs
+++ src/Epam.ASPCore.Northwind.Tests/CategoryServiceUnitTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Epam.ASPCore.Northwind.Domain.Models;
 using Epam.ASPCore.Northwind.Domain.Repositories;
@@ -5,6 +6,7 @@ using Epam.ASPCore.Northwind.WebUI.Controllers;
 using Epam.ASPCore.Northwind.WebUI.Models;
 using Epam.ASPCore.Northwind.WebUI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Moq;
 using Xunit;
 
@@ -29,6 +31,53 @@ namespace Epam.ASPCore.Northwind.Tests
             Assert.IsType<List<CategoriesModel>>(result);
             Assert.Single(result);
             Assert.Equal(1, result[0].CategoryId);
+        }
+
+        [Fact]
+        public void GetCategoriesList_WhenCalled_Exception()
+        {
+            // Arrange
+            var mockRepo = new Mock<INorthwindRepository<Categories>>();
+            mockRepo.Setup(repo => repo.Get())
+                .Throws(new ArgumentNullException());
+            var service = new CategoryService(mockRepo.Object);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => service.GetCategories());
+        }
+
+        [Fact]
+        public void GetCategoriesSelectedList_Test()
+        {
+            // Arrange
+            var mockRepo = new Mock<INorthwindRepository<Categories>>();
+            List<Categories> listCategories = new List<Categories> { new Categories { CategoryId = 1, CategoryName = "category1" } };
+            mockRepo.Setup(repo => repo.Get())
+                .Returns(() => listCategories);
+            var service = new CategoryService(mockRepo.Object);
+
+            // Act
+            var result = service.GetCategoriesSelectedList(1);
+
+            // Assert
+            Assert.IsType<List<SelectListItem>>(result);
+            Assert.Single(result);
+            Assert.Equal(listCategories[0].CategoryId.ToString(), result[0].Value);
+            Assert.Equal(listCategories[0].CategoryName, result[0].Text);
+            Assert.True(result[0].Selected);
+        }
+
+        [Fact]
+        public void GetCategoriesSelectedList_WhenCalled_Exception()
+        {
+            // Arrange
+            var mockRepo = new Mock<INorthwindRepository<Categories>>();
+            mockRepo.Setup(repo => repo.Get())
+                .Throws(new ArgumentNullException());
+            var service = new CategoryService(mockRepo.Object);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => service.GetCategoriesSelectedList());
         }
 
         [Fact]
