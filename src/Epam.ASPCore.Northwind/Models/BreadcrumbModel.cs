06a2555c2363bb541cd98c0aa85b349672f07a8e
Pavel <pavel.veselov@hotmail.com>
2019-10-27 01:08:11 +0300

    Create Breadcrumb View Component

---
+++ src/Epam.ASPCore.Northwind/Models/BreadcrumbModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Epam.ASPCore.Northwind.WebUI.Models
+{
+    public class BreadcrumbModel
+    {
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string EntityName { get; set; }
+        public bool IsCurrentPage { get; set; }
+    }
+}
---
+++ src/Epam.ASPCore.Northwind/ViewComponents/BreadcrumbsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Epam.ASPCore.Northwind.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Epam.ASPCore.Northwind.WebUI.ViewComponents
+{
+    public class BreadcrumbsViewComponent : ViewComponent
+    {
+        public Task<ViewViewComponentResult> InvokeAsync(List<BreadcrumbModel> listModels)
+        {
+            return Task.FromResult(View(listModels));
+        }
+    }
+}
